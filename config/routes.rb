adb72f79c823ee0941873072e9a714828a37837c
Evan Duncan <evan.e.duncan@gmail.com>
2020-11-30 20:36:47 -0600

    Add footer content

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  direct(:github) { "https://github.com/evan-duncan/exposing-nsbm" }
+  root 'dashboard#index'
 end
